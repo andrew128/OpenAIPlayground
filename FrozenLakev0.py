0278cf8c258af2a82e022d259fef107e3ae127ba
Andrew Wei <andrewwei98@gmail.com>
2018-08-12 17:40:44 -0400

    sorting code

--- FrozenLakev0.py
+++ FrozenLakev0.py
@@ -4,8 +4,8 @@ import numpy as np
 from random import *
 # ----------------------
 # Environment setup
-env = gym.make("Taxi-v2")
-#env = gym.make("FrozenLake-v0")
+#env = gym.make("Taxi-v2")
+env = gym.make("FrozenLake-v0")
 env.reset()
 # ----------------------
 # Use Q table to solve environment
@@ -18,20 +18,20 @@ G = 0
 # var for learning rate
 alpha = 0.1 #0.618
 # epsilon
-#e = 0.1
+e = 0.1
 # gamma
-#y = 0.95
+y = 0.95
 
 avg = 0
 for episode in range(1, 20001): # for each episode
     state, done, G, reward = env.reset(), False, 0, 0
     while done != True: # for each step of each episode
-#        prob = random()
-#        if prob > e:
+        prob = random()
+        if prob > e:
             # returns index of action of highest Q value for current state
-        action = np.argmax(Q[state]) 
-#        else:
-#            action = env.action_space.sample()
+            action = np.argmax(Q[state]) 
+        else:
+            action = env.action_space.sample()
         # action above is taken and we store new state as state2
         state2, reward, done, info = env.step(action)
         # bellman step:
